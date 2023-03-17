19c175a24d71c13a52163cd0315468b145d0d9de
daia_ <daiane.pguni@gmail.com>
2023-03-17 18:38:19 -0300

    Adding and setting up the App bar

--- app/src/main/java/br/com/marvelapi/MainActivity.kt
+++ app/src/main/java/br/com/marvelapi/MainActivity.kt
@@ -2,20 +2,19 @@ package br.com.marvelapi
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import br.com.marvelapi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var navHostFragment: NavHostFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.my_nav_host_fragment) as NavHostFragment
+        binding.topAppBar.setupWithNavController(findNavController(R.id.my_nav_host_fragment))
     }
 }
