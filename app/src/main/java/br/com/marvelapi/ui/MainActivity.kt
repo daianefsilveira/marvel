963223b9f353e6f717dae7828c8efdba780a33dd
daia_ <daiane.pguni@gmail.com>
2023-03-21 15:16:41 -0300

    Updating icons from mipmap folder, setting splash screen

--- app/src/main/java/br/com/marvelapi/ui/MainActivity.kt
+++ app/src/main/java/br/com/marvelapi/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package br.com.marvelapi.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import br.com.marvelapi.R
@@ -12,6 +13,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
